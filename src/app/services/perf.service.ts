d63788f8b9f0131e1d6ec02f5691716c7420f756
Richard Yin <yguobin@hotmail.com>
2022-11-08 05:32:29 -0500

    Updated host ips

--- src/app/services/perf.service.ts
+++ src/app/services/perf.service.ts
@@ -9,9 +9,9 @@ export class PerfService {
 
   ip_map: any = {
     'KafkaPerfPublisher-1': '34.23.73.75',
-    'KafkaPerfPublisher-2': '34.23.217.78',
+    'KafkaPerfPublisher-2': '34.23.187.170',
     'KafkaPerfConsumer-1': '35.231.141.111',
-    'KafkaPerfConsumer-2': '34.73.124.23'
+    'KafkaPerfConsumer-2': '34.23.217.78'
   };
 
   constructor(private http: HttpClient) { }
@@ -54,14 +54,6 @@ export class PerfService {
 
   setConsumerTopic(name: string, topic: string): Observable<any> {
     return this.http.post(`http://${this.ip_map[name]}:9401/topic`, topic);
-  }
-
-  getIP(name: string): string {
-    if( name === 'kafkaperfpublisher-1' ) return '34.23.73.75';
-    if( name === 'kafkaperfpublisher-2' ) return '34.23.217.78';
-    if( name === 'kafkaperfconsumer-1' ) return '35.231.141.111';
-    if( name === 'kafkaperfconsumer-2' ) return '34.73.124.23';
-    return '';
   }
 
 }
